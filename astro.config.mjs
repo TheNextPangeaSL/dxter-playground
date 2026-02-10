79ba70da546dad24dee1f38cf0570af8c0c66d48
Antonio Paya <antonio.paya@thenextpangea.com>
2026-02-10 14:41:21 +0100

    feature: added github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: "https://thenextpangeasl.github.io",
+  base: "/dxter-playground",
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
